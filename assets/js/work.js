9a79eda368486ba8b68036e9366030548344937a
Axel Olivas <axelbrolivas@gmail.com>
2021-10-20 22:02:41 -0700

    add work pages #TODO: add form logic and work pics/demos

--- assets/js/work.js
+++ assets/js/work.js
@@ -1,10 +1,9 @@
 let theme = localStorage.getItem('theme')
-let pic = localStorage.getItem('pic')
 
 if(theme == null){
    setTheme('light')
 } else{
-   setTheme(theme, pic)
+   setTheme(theme)
 }
 
 let themeDots = document.getElementsByClassName('theme-dot')
@@ -17,34 +16,26 @@ for(var i=0; themeDots.length>1; i++){
    })
 }
 
-function setTheme(mode, pic){
+function setTheme(mode){
    if(mode == 'light'){
       document.getElementById('theme-style').href = 'assets/css/index.css'
-      document.getElementById('spotify').src = 'assets/images/Spotify_Icon_RGB_Black.png'
    }
 
    if(mode == 'blue'){
       document.getElementById('theme-style').href = 'assets/css/blue.css'
-      document.getElementById('profile-pic').src = 'assets/images/AxelOlivas.jpg'
-      document.getElementById('spotify').src = 'assets/images/Spotify_Icon_RGB_Green.png'
    }
 
    if(mode == 'retro'){
       document.getElementById('theme-style').href = 'assets/css/retro.css'
-      document.getElementById('spotify').src = 'assets/images/Spotify_Icon_RGB_White.png'
    }
 
    if(mode == 'dark'){
       document.getElementById('theme-style').href = 'assets/css/dark.css'
-      document.getElementById('profile-pic').src = 'assets/images/Axel-intro.JPEG'
-      document.getElementById('spotify').src = 'assets/images/Spotify_Icon_RGB_Green.png'
    }
 
    if(mode == 'season'){
       document.getElementById('theme-style').href = 'assets/css/season.css'
-      document.getElementById('spotify').src = 'assets/images/Spotify_Icon_RGB_Season.png'
    }
 
    localStorage.setItem('theme', mode)
-   localStorage.setItem('pic', pic)
 }
